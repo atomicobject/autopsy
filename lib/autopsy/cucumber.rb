563e6bafbb8085b57992796190df3fb6c55d3e64
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-01-19 14:11:28 -0500

    fix cucumber artifact location

--- lib/autopsy/cucumber.rb
+++ lib/autopsy/cucumber.rb
@@ -5,7 +5,7 @@ Autopsy.artifacts_path = "./features/artifacts"
 
 After do |scenario|
   if scenario.failed?
-    file_base = "#{scenario.feature.file.split("/").last.split(".").first}-#{scenario.feature.line}"
+    file_base = "#{scenario.file.split("/").last.split(".").first}-#{scenario.line}"
     Autopsy.perform(page, file_base, scenario.exception)
   end
 end
