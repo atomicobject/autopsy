14b51864bef9d79ab2bb14425ef54be45a1445c2
Ryan Abel <ryan.abel@atomicobject.com>
2015-07-01 08:34:54 -0400

    Get console messages working for poltergeist

--- lib/autopsy.rb
+++ lib/autopsy.rb
@@ -6,20 +6,18 @@ require_relative 'autopsy/poltergeist'
 class Autopsy
   class << self
     attr_accessor :artifacts_path
+    attr_accessor :driver_console_messages_map
   end
 
-  @@driver_map = {
-    webkit: AutopsyWebkit,
-    poltergeist: AutopsyPoltergeist
-  }
+
 
   def self.perform(capybara_page, test_base, exception)
     file_base = "#{File.expand_path(Autopsy.artifacts_path)}/#{test_base}"
     if exception != nil
       capybara_page.save_screenshot "#{file_base}.png"
       capybara_page.save_page "#{file_base}.html"
-      if (@@driver_map[Capybara.current_driver])
-        msgs = @@driver_map[Capybara.current_driver].get_console_messages(capybara_page).ai(html: true)
+      if (Autopsy.driver_console_messages_map[Capybara.current_driver])
+        msgs = Autopsy.driver_console_messages_map[Capybara.current_driver].get_console_messages(capybara_page).ai(html: true)
         File.open("#{file_base}_msgs.html", 'w') {|f| f.write(msgs) }
       end
       File.open("#{file_base}_error.log", 'w') do |f|
@@ -35,4 +33,7 @@ class Autopsy
 end
 
 Autopsy.artifacts_path = "." # default
-
+Autopsy.driver_console_messages_map = {
+  webkit: AutopsyWebkit,
+  poltergeist: AutopsyPoltergeist
+}
--- lib/autopsy/poltergeist.rb
+++ lib/autopsy/poltergeist.rb
@@ -1,18 +1,17 @@
+require 'autopsy'
+
 class AutopsyPoltergeist
   class << self
     def init(name=:poltergeist, options = {})
+      Autopsy.driver_console_messages_map[name] = AutopsyPoltergeist
       @log = StringIO.new
       Capybara.register_driver name do |app|
-        Capybara::Poltergeist::Driver.new(app, options.merge(logger: @log))
+        Capybara::Poltergeist::Driver.new(app, options.merge(phantomjs_logger: @log))
       end
     end
 
     def get_console_messages(capybara_page)
-      lines = []
-      @log.each_line do |line|
-        lines << line
-      end
-      lines
+      @log.string.lines
     end
   end
 end
